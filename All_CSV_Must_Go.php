9f054edd069642f81ded5f8a07dab131c28f4cc1
Piotr Senkow <piotrsenkow@gmail.com>
2018-04-19 00:13:38 -0500

    cleaned up code

--- All_CSV_Must_Go.php
+++ All_CSV_Must_Go.php
@@ -1,88 +1,32 @@
 <?php
 //https://stackoverflow.com/questions/8538995/how-to-import-multiple-csv-files-into-a-mysql-database
-
-try
-{
+$table_names = array("L_as_L","L_as_R","R_as_L","R_as_R","vs_R","vs_L");
+try{
   $pdo = new PDO('mysql:host=localhost;dbname=Baseball', 'username', 'password');
   $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   $pdo->exec('SET NAMES "utf8"');
 }
-catch (PDOException $e)
-{
+catch (PDOException $e){
   $error = 'Unable to connect to the database server.';
   print($error);
   exit();
 }
-
-try {
-  $sql = 'Select * from L_as_L';
+try{
+foreach($table_names as $name){
+$files = glob("/opt/lampp/htdocs/fangraph_sql_updater/hello_friend/*".$name.".csv");
+foreach($files as $file){
+  $sql= "LOAD DATA INFILE '".$file." 'INTO TABLE ".$name." FIELDS TERMINATED BY ','
+  ENCLOSED BY  '\"'
+  LINES TERMINATED BY '\r\n'
+  IGNORE 1 LINES;";
   $result = $pdo -> query($sql);
-  print("you also found the table");
+  }
 }
-catch (PDOException $e)
-{
-  $error = 'Cant find the thing';
+print("You have succesfully loaded the database");
+}
+catch (PDOException $e){
+  $error = 'Something went horribly wrong';
   print($error);
   exit();
 }
-
-$files = glob("/opt/lampp/htdocs/fangraph_sql_updater/hello_friend/*L_as_L.csv");
-foreach($files as $file)
-{
-  $sql= "LOAD DATA INFILE '".$file." 'INTO TABLE L_as_L FIELDS TERMINATED BY ','
-  ENCLOSED BY  '\"'
-  LINES TERMINATED BY '\r\n'
-  IGNORE 1 LINES;";
-  $result = $pdo -> query($sql);
-}
-
-$files = glob("/opt/lampp/htdocs/fangraph_sql_updater/hello_friend/*L_as_R.csv");
-foreach($files as $file)
-{
-  $sql= "LOAD DATA INFILE '".$file." 'INTO TABLE L_as_R FIELDS TERMINATED BY ','
-  ENCLOSED BY  '\"'
-  LINES TERMINATED BY '\r\n'
-  IGNORE 1 LINES;";
-  $result = $pdo -> query($sql);
-}
-
-$files = glob("/opt/lampp/htdocs/fangraph_sql_updater/hello_friend/*R_as_L.csv");
-foreach($files as $file)
-{
-  $sql= "LOAD DATA INFILE '".$file." 'INTO TABLE R_as_L FIELDS TERMINATED BY ','
-  ENCLOSED BY  '\"'
-  LINES TERMINATED BY '\r\n'
-  IGNORE 1 LINES;";
-  $result = $pdo -> query($sql);
-}
-
-$files = glob("/opt/lampp/htdocs/fangraph_sql_updater/hello_friend/*vs_R.csv");
-foreach($files as $file)
-{
-  $sql= "LOAD DATA INFILE '".$file." 'INTO TABLE vs_R FIELDS TERMINATED BY ','
-  ENCLOSED BY  '\"'
-  LINES TERMINATED BY '\r\n'
-  IGNORE 1 LINES;";
-  $result = $pdo -> query($sql);
-}
-
-$files = glob("/opt/lampp/htdocs/fangraph_sql_updater/hello_friend/*vs_L.csv");
-foreach($files as $file)
-{
-  $sql= "LOAD DATA INFILE '".$file." 'INTO TABLE vs_L FIELDS TERMINATED BY ','
-  ENCLOSED BY  '\"'
-  LINES TERMINATED BY '\r\n'
-  IGNORE 1 LINES;";
-  $result = $pdo -> query($sql);
-}
-/*
-LOAD DATA INFILE '/home/piotr/Desktop/hello_friend/Angels_vs_L_as_L.csv'
-into TABLE L_as_L
-FIELDS TERMINATED BY ','
-ENCLOSED BY  '\"'
-LINES TERMINATED BY "\r\n"
-IGNORE 1 LINES;
-*/
-
-
  ?>
